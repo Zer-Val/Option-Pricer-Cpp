b85b30ad45826e86c1ffca624e586b110beb042d
ZerVal <156453793+Zer-Val@users.noreply.github.com>
2024-12-16 00:50:53 +0100

    Update AsianPutOption.h
    Update commentaires

--- ProjetC++/AsianPutOption.h
+++ ProjetC++/AsianPutOption.h
@@ -3,13 +3,13 @@
 
 class AsianPutOption : public AsianOption 
 {
-    public:
-	    // Constructor that initializes _strike
-        AsianPutOption(const std::vector<double>& timeSteps, double strike);
+	public:
+		//Constructor of AsianPutOption, that use the constructor of AsianOption and initialize _strike as a double (K)
+        	AsianPutOption(const std::vector<double>& timeSteps, double strike);
 
-        // Implementation of the payoff method for the Put option
-        double payoff(double underlyingPrice) const override;
+        	// Implementation of the payoff method for the Put option
+        	double payoff(double underlyingPrice) const override;
 
-    private:
-        double _strike; //Private member to store the strike price of the option
+    	private:
+        	double _strike; //Private member to store the strike price of the option
 };
