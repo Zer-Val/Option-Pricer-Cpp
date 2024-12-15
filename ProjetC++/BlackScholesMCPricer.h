edfff3d18b5297de620ec4af763330a7823de68e
ZerVal <156453793+Zer-Val@users.noreply.github.com>
2024-12-16 00:59:10 +0100

    Update BlackScholesMCPricer.h

--- ProjetC++/BlackScholesMCPricer.h
+++ ProjetC++/BlackScholesMCPricer.h
@@ -8,34 +8,31 @@
 
 class BlackScholesMCPricer 
 {
-    public:
+	public:
 		// Constructor of the BlackScholesMCPricer class
-        BlackScholesMCPricer(Option* option, double initial_price, double interest_rate, double volatility);
+        	BlackScholesMCPricer(Option* option, double initial_price, double interest_rate, double volatility);
 
 		// Method to get the number of paths generated sicne the beginning of the program
 		int getNbPaths() const;
 
 		// Method to generate paths
-        void generate(int nb_paths);
+        	void generate(int nb_paths);
 
 		// Operator () to price the option
-        double operator()() const;
+        	double operator()() const;
 
 		// Method to compute the 95% confidence interval of theprice
-        std::vector<double> confidenceInterval() const;
+        	std::vector<double> confidenceInterval() const;
 
+   	 private:
 
-
-    private:
-        Option* _option; 
+        	Option* _option; 
 		double _underlying_price; // Underlying asset price (S0)
 		double _risk_free_rate; // Risk-free interest rate (r)
 		double _volatility; // Volatility (sigma)
-        int _nb_paths_generated; // Number of paths already generated since the beginning of the program (Nb)
+        	int _nb_paths_generated; // Number of paths already generated since the beginning of the program (Nb)
 		double _current_estimate; // Current estimate of the option price
-
 		double estimates_mean; //
-        double estimates_variance; //
-
-        std::vector<double> estimated_prices; // Store estimated option prices
+        	double estimates_variance; //
+        	std::vector<double> estimated_prices; // Store estimated option prices
 };
