1a6acf7dd3ec56abd3eadf0ad3f7a5fa0958e1cc
ZerVal <156453793+Zer-Val@users.noreply.github.com>
2024-12-16 10:55:25 +0100

    Update BlackScholesPricer.h

--- ProjetC++/BlackScholesPricer.h
+++ ProjetC++/BlackScholesPricer.h
@@ -8,20 +8,19 @@ class BlackScholesPricer
 	public:
 		//Constructor of BlackScholesPricer - for EuropeanVanillaOptions
 		BlackScholesPricer(EuropeanVanillaOption* option, double asset_price, double interest_rate, double volatility);
-		
+
 		//Constructor of BlackScholesPricer - for EuropeanDigitalOptions
-		BlackScholesPricer(EuropeanDigitalOption* option, double asset_price, double interest_rate, double volatility); 
-
+		BlackScholesPricer(EuropeanDigitalOption* option, double asset_price, double interest_rate, double volatility);
+		
 		//Operator () to price the option
-		double operator()() const; 
-
+		double operator()() const;
+		
 		//Method to compute the delta of the option
 		double delta() const; 
 
 	private:
-
-		EuropeanVanillaOption* vanilla_option_; // Pointer to the vanilla option we want to price
-		EuropeanDigitalOption* digital_option_; // Pointer to the digital option we want to price
+		EuropeanVanillaOption* _vanilla_option; // Pointer to the vanilla option we want to price
+		EuropeanDigitalOption* _digital_option; // Pointer to the digital option we want to price
 		double _asset_price; // Underlying asset price (S)
 		double _interest_rate; // Risk-free interest rate (r)
 		double _volatility; // Volatility (vol)
