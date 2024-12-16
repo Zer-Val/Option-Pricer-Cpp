c19278ed37d741dd5175240ccb0e12ee789a1424
ZerVal <156453793+Zer-Val@users.noreply.github.com>
2024-12-16 14:08:01 +0100

    Update MT.h

--- ProjetC++/MT.h
+++ ProjetC++/MT.h
@@ -1,25 +1,25 @@
 #pragma once
 #include <random>
 
-class MT 
+class MT
 {
-	public:
-		// Method to get the unique instance of the class
-		static MT& getInstance();
+public:
+	// Method to get the unique instance of the class
+	static MT& getInstance();
 
-		// Method to generate a random number between 0 and 1
-		static double rand_unif();
+	// Method to generate a random number between 0 and 1
+	static double rand_unif();
 
-		// Method to generate a random number following a normal distribution
-		static double rand_norm();
+	// Method to generate a random number following a normal distribution
+	static double rand_norm();
 
-	private:
-		// Private constructor to prevent instanciation
-		MT();
+private:
+	// Private constructor to prevent instanciation
+	MT();
 
-		// Deleted copy constructor and assignment operator to prevent copies
-		MT(const MT&) = delete;
-		MT& operator=(const MT&) = delete;
+	// Deleted copy constructor and assignment operator to prevent copies
+	MT(const MT&) = delete;
+	MT& operator=(const MT&) = delete;
 
-		std::mt19937 mt; // Pseudo-random number generator
+	std::mt19937 mt; // Pseudo-random number generator
 };
