360de8e5c537a9a9e24a6333c3c7fa0392153f89
ZerVal <156453793+Zer-Val@users.noreply.github.com>
2024-12-16 14:20:18 +0100

    Update PutOption.cpp

--- ProjetC++/PutOption.cpp
+++ ProjetC++/PutOption.cpp
@@ -1,13 +1,12 @@
 #include "PutOption.h"
 
-//Implementation of the constructor of (EuropeanVanilla)PutOption, that use the constructor of EuropeanVanillaOption
+// Implementation of the constructor for PutOption, uses the one from EuropeanVanillaOption
 PutOption::PutOption(double expiry, double strike) : EuropeanVanillaOption(expiry, strike) {}
 
 //Implementation of the Payoff method for an European Vanilla Put option
 double PutOption::payoff(double z) const
 {
-	return (getStrike() >= z) ? (getStrike() - z) : 0.0; 	// ? is a ternary operator, which I use to write if-else conditions in a single line. 
-								// Here, (z >= getStrike()) ? (z - getStrike()) : 0.0 <=> if (z >= getStrike()) { return z - getStrike(); } else { return 0.0; }
+	return (getStrike() >= z) ? (getStrike() - z) : 0.0; // ? est un opérateur ternaire, je m'en sers pour écrire des conditions if-else en une seule ligne ici (getStrike() >= z) ? (getStrike() - z) : 0.0 <=> if (getStrike() >= z) { return getStrike() - z; } else { return 0.0; }
 }
 
 //Implementation of the GetOptionType method for an European Vanilla Put option
