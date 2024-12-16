49ad516587f653eb7a452c90894688f45552607f
ZerVal <156453793+Zer-Val@users.noreply.github.com>
2024-12-16 14:11:36 +0100

    Update EuropeanDigitalOption.cpp

--- ProjetC++/EuropeanDigitalOption.cpp
+++ ProjetC++/EuropeanDigitalOption.cpp
@@ -1,16 +1,16 @@
 #include "EuropeanDigitalOption.h"
 
 //Implementation of the constructor of EuropeanDigitalOption, that use the constructor of Option and initialize _strike as a double (K)
-EuropeanDigitalOption::EuropeanDigitalOption(double K, double expiry) : Option(expiry), _strike(K) {}
+EuropeanDigitalOption::EuropeanDigitalOption(double expiry, double K) : Option(expiry), _strike(K) {}
 
 // Getter method for _strikedouble EuropeanDigitalOption::getStrike() const
-double EuropeanDigitalOption::getStrike() const 
+double EuropeanDigitalOption::getStrike() const
 {
-    return _strike;
+	return _strike;
 }
 
 // Override of isAsianOption to return false for EVDO - (Compilation issue if not)
-bool EuropeanDigitalOption::isAsianOption() const 
+bool EuropeanDigitalOption::isAsianOption() const
 {
 	return false;
 }
