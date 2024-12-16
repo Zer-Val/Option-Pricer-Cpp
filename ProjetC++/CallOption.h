1c33c6c20db6b9ce1d1ceb049de1bfab390af4fe
ZerVal <156453793+Zer-Val@users.noreply.github.com>
2024-12-16 14:04:57 +0100

    Update CallOption.h

--- ProjetC++/CallOption.h
+++ ProjetC++/CallOption.h
@@ -3,13 +3,13 @@
 
 class CallOption : public EuropeanVanillaOption
 {
-	public:
-		//Constructor of (EuropeanVanilla)CallOption, that use the constructor of EuropeanVanillaOption
-        	CallOption(double expiry, double strike);
+public:
+    //Constructor of (EuropeanVanilla)CallOption, that use the constructor of EuropeanVanillaOption
+    CallOption(double expiry, double strike);
 
-        	//Override of the payoff method for an European Vanilla Call option
-        	double payoff(double z) const override;
+    //Override of the payoff method for an European Vanilla Call option
+    double payoff(double z) const override;
 
-        	//GetOptionType method for an European Vanilla Call option
-        	optionType GetOptionType() const override;
+    //GetOptionType method for an European Vanilla Call option
+    optionType GetOptionType() const override;
 };
