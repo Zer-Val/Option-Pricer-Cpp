4867008b3d7967dd0153c971d0ab0ee48381aee5
ZerVal <156453793+Zer-Val@users.noreply.github.com>
2024-12-16 13:57:48 +0100

    Update AmericanCallOption.h

--- ProjetC++/AmericanCallOption.h
+++ ProjetC++/AmericanCallOption.h
@@ -3,13 +3,13 @@
 
 class AmericanCallOption : public AmericanOption
 {
-	public:
-		//Constructor of AmericanCallOption, that use the constructor of AmericanOption
-		AmericanCallOption(double expiry, double strike);
+public:
+	//Constructor of AmericanCallOption, that use the constructor of AmericanOption
+	AmericanCallOption(double expiry, double strike);
 
-		//Payoff method for an American Call option
-		double payoff(double z) const override;
+	//Payoff method for an American Call option
+	double payoff(double z) const override;
 
-		//GetOptionType method for a(n) (American) Call option
-		optionType GetOptionType() const override;
+	//GetOptionType method for a(n) (American) Call option
+	optionType GetOptionType() const override;
 };
