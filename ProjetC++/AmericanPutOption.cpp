9081f80aacdbad9b6bad320757fbff56f8818614
ZerVal <156453793+Zer-Val@users.noreply.github.com>
2024-12-16 09:25:09 +0100

    Update AmericanPutOption.cpp

--- ProjetC++/AmericanPutOption.cpp
+++ ProjetC++/AmericanPutOption.cpp
@@ -1,10 +1,13 @@
 #include "AmericanPutOption.h"
 
-//Implementation of the constructor for AmericanCallOption, uses the one from AmericanOption
+//Implementation of constructor of AmericanPutOption, that use the constructor of AmericanOption
 AmericanPutOption::AmericanPutOption(double expiry, double strike) : AmericanOption(expiry, strike) {}
 
 //Implementation of the Payoff method for an European Vanilla Call option
-
+double AmericanPutOption::payoff(double z) const
+{
+	return (getStrike() >= z) ? (getStrike() - z) : 0.0;
+}
 
 //Implementation of the GetOptionType method for a Call option
 AmericanPutOption::optionType AmericanPutOption::GetOptionType() const
