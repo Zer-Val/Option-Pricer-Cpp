b000c5f29d98c37c955f8d9832fa822004e6a651
ZerVal <156453793+Zer-Val@users.noreply.github.com>
2024-12-16 09:20:33 +0100

    Update AmericanOption.cpp

--- ProjetC++/AmericanOption.cpp
+++ ProjetC++/AmericanOption.cpp
@@ -1,6 +1,6 @@
 #include "AmericanOption.h"
 
-//Implementation of the constructor of AmericanCallOption, that use the constructor of AmericanOption
+//Implementation of constructor of AmericanOption, that use the constructor of Option and initialize _strike as a double (K)
 AmericanOption::AmericanOption(double expiry, double strike) : Option(expiry), _strike(strike) {}
 
 // Getter method for _strike
