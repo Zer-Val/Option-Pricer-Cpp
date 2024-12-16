a07199726074ccbd10392a5c83123f261b719db2
ZerVal <156453793+Zer-Val@users.noreply.github.com>
2024-12-16 15:03:42 +0100

    Update AsianCallOption.cpp

--- ProjetC++/AsianCallOption.cpp
+++ ProjetC++/AsianCallOption.cpp
@@ -5,7 +5,13 @@
 AsianCallOption::AsianCallOption(const std::vector<double>& timeSteps, double strikePrice) : AsianOption(timeSteps), _strike(strikePrice) {}
 
 // Implementation of the payoff method for the Call option
-double AsianCallOption::payoff(double underlyingPrice) const 
+double AsianCallOption::payoff(double underlyingPrice) const
 {
     return std::max(underlyingPrice - _strike, 0.0);
+}
+
+//Implementation of the GetOptionType method for an Asian Call option
+AsianOption::optionType AsianCallOption::GetOptionType() const
+{
+	return optionType::call;
 }
