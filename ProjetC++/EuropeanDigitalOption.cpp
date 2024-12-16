f09515ebe02ef65a05d998139ac167970b48b36a
ZerVal <156453793+Zer-Val@users.noreply.github.com>
2024-12-16 11:08:49 +0100

    Update EuropeanDigitalOption.cpp

--- ProjetC++/EuropeanDigitalOption.cpp
+++ ProjetC++/EuropeanDigitalOption.cpp
@@ -1,13 +1,7 @@
 #include "EuropeanDigitalOption.h"
 
 //Implementation of the constructor of EuropeanDigitalOption, that use the constructor of Option and initialize _strike as a double (K)
-EuropeanDigitalOption::EuropeanDigitalOption(double K, double expiry) : Option(expiry), strike(K) 
-{
-    if (expiry < 0 || strike < 0) 
-    {
-        throw std::invalid_argument("Expiry and strike must be non-negative");
-    }
-}
+EuropeanDigitalOption::EuropeanDigitalOption(double K, double expiry) : Option(expiry), strike(K) {}
 
 // Getter method for _strikedouble EuropeanDigitalOption::getStrike() const
 double EuropeanDigitalOption::getStrike() const 
