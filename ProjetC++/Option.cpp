c86c9b26ab1d518d7a52e1352499f8a2a892788c
ZerVal <156453793+Zer-Val@users.noreply.github.com>
2024-12-16 14:10:23 +0100

    Update Option.cpp

--- ProjetC++/Option.cpp
+++ ProjetC++/Option.cpp
@@ -5,25 +5,25 @@
 Option::Option(double expiry) : _expiry(expiry) {}
 
 // Getter method for _expiry
-double Option::getExpiry() const 
+double Option::getExpiry() const
 {
 	return _expiry;
 }
 
 // Implementation of the method payoffPath that calls the method payoff(double) with the last element of the vector
-double Option::payoffPath(const std::vector<double>& prices) const 
+double Option::payoffPath(const std::vector<double>& prices) const
 {
 	return payoff(prices.back()); // Call the payoff method with the last element of the vector
 }
 
 // Implementation of the virtual method to check if the option is an Asian Option
-bool Option::isAsianOption() const 
+bool Option::isAsianOption() const
 {
-    	return false;
+	return false;
 }
 
 // Implementation of the virtual method to check if the option is an American Option
-bool Option::isAmericanOption() const 
+bool Option::isAmericanOption() const
 {
 	return false;
 }
