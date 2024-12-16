432f67159deb12e537a42e9c5bcb1b2ae5123a04
ZerVal <156453793+Zer-Val@users.noreply.github.com>
2024-12-16 01:14:58 +0100

    Update PutOption.h

--- ProjetC++/PutOption.h
+++ ProjetC++/PutOption.h
@@ -4,12 +4,12 @@
 class PutOption : public EuropeanVanillaOption
 {
     public:
-        //Constructor for PutOption, uses the one from EuropeanVanillaOption
+        //Constructor of (EuropeanVanilla)PutOption, that use the constructor of EuropeanVanillaOption
         PutOption(double expiry, double strike);
-
+        
         //Payoff method for an European Vanilla Put option
         double payoff(double z) const override;
-
+        
         //GetOptionType method for an European Vanilla Put option
         optionType GetOptionType() const override;
 };
